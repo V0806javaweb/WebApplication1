e0c0a8e113e47d23e441b4ca136cb3bfff885074
柏維 陳 <tp61i6jo6168@gmail.com>
2024-03-19 11:35:22 +0800

    2024/03/19

---
+++ Models/TestModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class TestModel
+    {
+        [Required(ErrorMessage = "輸入用戶名稱")]
+        [Display(Name = "用戶名稱:")]
+        public string txtId { get; set; }
+
+        [Required(ErrorMessage = "輸入用戶密碼")]
+        [Display(Name = "用戶密碼:")]
+        public string txtPasswd { get; set; }
+
+        public bool Remeber { get; set; }
+        public string txtRemember { get; set; }
+    }
+}
